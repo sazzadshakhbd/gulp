9d36f1f6562b1ff397d518735da83f24624eef14
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-10-09 23:56:06 +0600

    css js converstion done

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,9 +1,31 @@
-(function ($) {
-  "use strict"; // Start of use strict
-
-  $(document).ready(function () {
-   
-    console.log("script here")
-
+// Navbar on scroll animation
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 100) {
+      document.getElementById("navbar_top").classList.add("fixed-top");
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector(".navbar").offsetHeight;
+      document.body.style.paddingTop = navbar_height + "px";
+    } else {
+      document.getElementById("navbar_top").classList.remove("fixed-top");
+      // remove padding top from body
+      document.body.style.paddingTop = "0";
+    }
   });
-})(jQuery); // End of use strict
+});
+// Wow js initialize
+$(function () {
+  new WOW().init();
+});
+// Add active class
+var x = document.getElementById("main-nav");
+var y = x.getElementsByClassName("nav-item");
+for (var i = 0; i < y.length; i++) {
+  y[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("activeClass");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" activeClass", "");
+    }
+    this.className += " activeClass";
+  });
+}
